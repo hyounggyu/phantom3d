349948fe4a97b8539587a581c9fc32a2ebef1c33
Hyounggyu Kim <hgkim10@gmail.com>
2015-05-20 10:53:04 +0900

    Update run script

--- run.m
+++ run.m
@@ -1,5 +1,16 @@
+#!/usr/local/bin/MathematicaScript -script
+
 <<Phantom3D`
 
-regions = Table[Phantom3DTransformedRegion[{RotationTransform[th, {0, 0, 1}]}], {th, 0, Pi, Pi/180}];
-im = ImageAdd@Image3DSlices[Phantom3DRegionToImage3D[#, 400], All, 3]& /@ regions 
-Export[FileNameJoin[{Environment["PWD"], "im.h5"}], ImageData/@im];
+regions = Table[Phantom3DTransformedRegion[{RotationTransform[th, {0, 0, 1}],
+            TranslationTransform[{0.5 Cos[th], 0.5 Sin[th], 0.2 Sin[th]}]}], {th, 0, Pi, Pi/90}]
+
+Block[{max = 1.5, i},
+ For[i = 1, i <= Length[regions], i++,
+  Print@DateList[TimeZone -> 9];
+  Print[i];
+  image = Phantom3DRegionToImage3D[regions[[i]], 420, max];
+  Export["data" <> ToString [i] <> ".h5", ImageData@image];
+  Clear[image];
+ ]
+]
